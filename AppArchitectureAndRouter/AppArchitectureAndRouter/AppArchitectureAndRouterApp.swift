9278810f4a88fcc5ef263096bacb708bb06191c4
정진우 <wlsdn4808@naver.com>
2025-02-18 16:07:26 +0900

    feat: initial commit

---
+++ AppArchitectureAndRouter/AppArchitectureAndRouter/AppArchitectureAndRouterApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppArchitectureAndRouterApp.swift
+//  AppArchitectureAndRouter
+//
+//  Created by inforex on 2/18/25.
+//
+
+import SwiftUI
+
+@main
+struct AppArchitectureAndRouterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppArchitectureAndRouter/AppArchitectureAndRouterTests/AppArchitectureAndRouterTests.swift
@@ -0,0 +1,16 @@
+//
+//  AppArchitectureAndRouterTests.swift
+//  AppArchitectureAndRouterTests
+//
+//  Created by inforex on 2/18/25.
+//
+
+import Testing
+
+struct AppArchitectureAndRouterTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
